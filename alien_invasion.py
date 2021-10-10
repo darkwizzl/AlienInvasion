07d08515d54ad1728fc432ed1d96f01e763d2c2e
darkwizzl <harikrishnansunil120@gmail.com>
2021-10-10 21:23:40 +0530

    added main file

---
+++ alien_invasion.py
@@ -0,0 +1,20 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((800,600))
+        pygame.display.set_caption("Alien Invasion")
+    
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
+        
+ai = AlienInvasion()
+ai.run_game()
