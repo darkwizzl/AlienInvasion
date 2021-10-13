032d74ba6ba2c08340125f9f2ac17862d1291053
darkwizzl <harikrishnansunil120@gmail.com>
2021-10-13 21:56:13 +0530

    edit some files

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,24 +3,46 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))
+        self.screen = pygame.display.set_mode(
+            (self.settings.width, self.settings.height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
-
     def run_game(self):
         while True:
-            self.screen.fill(self.settings.bg) 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.ship.blitme() 
-            pygame.display.flip()
-        
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move ship to right
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False    
+
+
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
 ai = AlienInvasion()
 ai.run_game()
--- ship.py
+++ ship.py
@@ -3,13 +3,25 @@ import pygame
 class Ship:
     def __init__(self,ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         #load player image and get its rect
-        self.image = pygame.image.load('images/PLAYER.png')
+        self.image = pygame.image.load('/home/bharadwaj/Desktop/test/AlienInvasion/images/PLAYER.png')
         self.rect = self.image.get_rect()
-
         self.rect.midbottom = self.screen_rect.midbottom
+        #movement
+        self.moving_right = False
+        self.moving_left = False 
+
+        self.x = float(self.rect.x) 
+
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
-    
+    def update(self):
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
