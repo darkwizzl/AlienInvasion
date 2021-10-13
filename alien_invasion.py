164c6839c9be8557bf34130595a74ccbf5b7c74f
darkwizzl <harikrishnansunil120@gmail.com>
2021-10-13 08:30:37 +0530

    fixed bugs

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
@@ -8,14 +10,15 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))
         pygame.display.set_caption("Alien Invasion")
-    
+        self.ship = Ship(self)
+
 
     def run_game(self):
         while True:
-            self.screen.fill(self.settings.bg)
+            self.screen.fill(self.settings.bg) 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    sys.exit()
+                    sys.exit()  
             pygame.display.flip()
         
 ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -3,7 +3,7 @@ import pygame
 class Ship:
     def __init__(self,ai_game):
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen_rect()
+        self.screen_rect = ai_game.screen.get_rect()
         #load player image and get its rect
         self.image = pygame.image.load('images/player.png')
         self.rect = self.image.get_rect()
